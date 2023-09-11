fbb72576928a5b3d58a17b5ea7b42e014e3115d5
Salando Linux <saulorlandozzz@gmail.com>
2023-09-10 23:37:37 -0600

    build: :arrow_down: se descargo gulp para pug y se ordeno la estructura de carpetas de mejor manera

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp')
+const pug = require('gulp-pug')
+
+gulp.task('pug', () => {
+  return gulp
+    .src('./src/pages/*.pug')
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./dist/html'))
+})
+
+gulp.task('pug-index', () => {
+  return gulp
+    .src('./*.pug')
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./'))
+})
+
+gulp.task('default', () => {
+  gulp.watch('./src/pages/*.pug', gulp.series('pug'))
+  gulp.watch('./*pug', gulp.series('pug-index'))
+})
+
